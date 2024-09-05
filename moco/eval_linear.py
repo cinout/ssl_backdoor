a30a40317494a8e903dd387bc68ab8f02c573411
seaskyho <seaskyho@gmail.com>
2024-09-05 17:59:34 +1000

    update

--- moco/eval_linear.py
+++ moco/eval_linear.py
@@ -564,14 +564,18 @@ def generate_evalaution_results(
     imagenet_metadata_dict,
     class_dir_list,
 ):
-    print(f">>>>>> evaluating clean validation set")
-    acc1, _, conf_matrix_clean = validate_conf_matrix(
-        val_loader, backbone, linear, args
-    )
+    # TODO: uncommet
+    # print(f">>>>>> evaluating clean validation set")
+    # acc1, _, conf_matrix_clean = validate_conf_matrix(
+    #     val_loader, backbone, linear, args
+    # )
     print(f">>>>>> evaluating poisoned validation set")
     acc1_p, _, conf_matrix_poisoned = validate_conf_matrix(
         val_poisoned_loader, backbone, linear, args
     )
+    # TODO: remove
+
+    exit()
 
     if args.detect_trigger_channels:
         for k in args.channel_num:
--- mytest.py
+++ mytest.py
@@ -12,26 +12,14 @@ import matplotlib.pyplot as plt
 from collections import Counter
 import time
 
-# max_indices_at_channel = np.random.randint(0, 20, size=(4, 10))
-# this_bs, num_views = max_indices_at_channel.shape
-# print(max_indices_at_channel)
+visited = 0
 
-# entropies = []
 
-# for votes in max_indices_at_channel:
-#     votes_counter = Counter(votes).most_common()
-#     counts = np.array([c for (name, c) in votes_counter])
-#     print(counts)
-#     p = counts / counts.sum()
-#     h = -np.sum(p * np.log(p))
-#     entropy = np.exp(h)
-#     entropies.append(entropy)
+def change_visited():
+    global visited
+    print(visited)
+    visited += 1
 
-# print(f">>>> entropies at channel are: {[round(e,2) for e in entropies]}")
-# entropies = np.array(entropies)
-# min_index = np.argmin(entropies)
-# print(min_index)
 
-a = [2, 3, 5, 1, 2]
-
-print(a[-2:])
+change_visited()
+change_visited()
